bc305e0e73dd4c7e8211f57e2ee6086d3d5474c7
xinyuww <36341353+xinyuww@users.noreply.github.com>
2019-04-12 16:42:23 -0500

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -7,5 +7,5 @@ par = {
 	'output_dir':	'', # (str) your output excel file directory
 	'start_num':	0, # (int) subject number that you want to start with
 	'end_num':		0, # (int) subject number that you want to end with
-	'login_sleep'	5 # (int in seconds) sleep time after clicking login, set longer to allow robot check
+	'login_sleep':	5 # (int in seconds) sleep time after clicking login, set longer to allow robot check
 }
